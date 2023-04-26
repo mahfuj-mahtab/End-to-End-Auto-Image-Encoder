d4f12671f880b52b5644e95a7d125eb12bd4276e
Mahfuj Mahtab Mohot <mohotmohot1@gmail.com>
2023-04-27 01:07:33 +0600

    Done

--- encrypter/encrypter/settings.py
+++ encrypter/encrypter/settings.py
@@ -138,3 +138,6 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587  
 EMAIL_HOST_USER = 'itstechnerd@gmail.com'  
 EMAIL_HOST_PASSWORD = 'ajwaekbybukqlznb'  
+
+MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
--- encrypter/encrypter/urls.py
+++ encrypter/encrypter/urls.py
@@ -15,6 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+
 from endtoendencryption.views import *
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -22,6 +25,9 @@ urlpatterns = [
     path('account/register',register,name='registration'),
     path('account/login',loginfunc,name='login'),
     path('account/profile/verify',verifyprofilefunc,name = 'verify profile'),
-    path('decode',decodefunc,name = 'decode function'),
+    path('encode/',encodefunc,name = 'encode function'),
+    path('decode/',decodefunc,name = 'decode function'),
+    # path('decoded/',decodedfunc,name = 'decoded function'),
     path('logout',logout_view,name = 'logout function'),
-]
+] + static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
